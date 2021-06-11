054f2d016cfcb974653c76e04637063873132625
Carter Tsai <hamming1@gmail.com>
2021-06-11 23:31:13 +0800

    update js

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -171,15 +171,15 @@ function registerButtonHandlers() {
     // });
 
     // get access token
-    // document.getElementById('getAccessToken').addEventListener('click', function() {
-    //     if (!liff.isLoggedIn() && !liff.isInClient()) {
-    //         alert('To get an access token, you need to be logged in. Please tap the "login" button below and try again.');
-    //     } else {
-    //         const accessToken = liff.getAccessToken();
-    //         document.getElementById('accessTokenField').textContent = accessToken;
-    //         toggleAccessToken();
-    //     }
-    // });
+    document.getElementById('getAccessToken').addEventListener('click', function() {
+        if (!liff.isLoggedIn() && !liff.isInClient()) {
+            alert('To get an access token, you need to be logged in. Please tap the "login" button below and try again.');
+        } else {
+            const accessToken = liff.getAccessToken();
+            document.getElementById('accessTokenField').textContent = accessToken;
+            toggleAccessToken();
+        }
+    });
 
     // get profile call
     // document.getElementById('getProfileButton').addEventListener('click', function() {
